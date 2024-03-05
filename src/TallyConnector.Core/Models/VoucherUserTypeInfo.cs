0bb21ee5cb5805ad5c9d053787390a8a8abd1082
aquib-softgen <softgen.aquib.shaikh@gmail.com>
2024-03-05 10:49:40 +0530

    Add VoucherUserTypeInfo model

---
+++ src/TallyConnector.Core/Models/VoucherUserTypeInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallyConnector.Core.Models
+{
+    public class VoucherUserTypeInfo
+    {
+        public string GUID { get; set; }
+        public string VoucherNumber { get; set; }
+        public string BrokerName { get; set; }
+        public decimal? Brokerage { get; set; }  
+        public decimal? TotalBrokerCommission { get; set; }  
+    }
+}
