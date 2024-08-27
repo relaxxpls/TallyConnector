5ae0e5f39e09598be67102c5ec4ef67a7e626f90
aquib-softgen <softgen.aquib.shaikh@gmail.com>
2024-08-27 12:06:25 +0530

    Add Hardware info class

--- src/TallyConnector.Core/Models/VoucherUserTypeInfo.cs
+++ src/TallyConnector.Core/Models/VoucherUserTypeInfo.cs
@@ -1,22 +1,26 @@
-﻿using MongoDB.Bson;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 
 namespace TallyConnector.Core.Models
 {
     public class VoucherUserTypeInfo
     {
-        public ObjectId Id 
-        {
-            get; set;
-        }
+        public ObjectId Id { get; set; }
         public string VoucherId { get; set; }
         public string BillName { get; set; }
+
         public string BrokerName { get; set; }
-        public decimal? Brokerage { get; set; }  
-        public decimal? TotalBrokerCommission { get; set; }  
+        public decimal? Brokerage { get; set; }
+
+        public string? SalesPerson { get; set; }
+        public decimal? Incentive { get; set; }
+
+        public decimal? TotalSalesIncentive { get; set; }
+
+        public decimal? TotalBrokerCommission { get; set; }
     }
 }
